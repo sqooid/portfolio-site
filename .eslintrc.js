b88ebf651d352f2e522af00e16ec57b748a4de11
Lucas Liu <lucasliu7@hotmail.com>
2022-05-05 19:08:23 +1000

    fixed configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    'vue/setup-compiler-macros': true,
   },
   extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
   plugins: ['vue', '@typescript-eslint'],
@@ -13,10 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/script-setup-uses-vars': 'error',
-  },
-  globals: {
-    defineProps: 'readonly',
-    defineEmits: 'readonly',
-    withDefaults: 'readonly',
+    'vue/multi-word-component-names': 'off',
   },
 }
